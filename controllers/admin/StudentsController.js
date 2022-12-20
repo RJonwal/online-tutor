07ce22d8101f116cb96aa65b60ad82816b274de4
ramkumawathis <rakeshjonwal.his@gmail.com>
2022-12-20 16:28:00 +0530

    ajax datatable on students

--- controllers/admin/StudentsController.js
+++ controllers/admin/StudentsController.js
@@ -2,11 +2,10 @@
 const User = require('../../models/user');
 const School = require('../../models/School');
 const Grade = require('../../models/Grade');
-
 const moment = require('moment');
-
 const fs = require('fs');
 let session = require('express-session');
+
 module.exports = {
     index,
     create,
@@ -14,9 +13,10 @@ module.exports = {
     edit,
     update,
     destroy,
-    datatable,
+    dataTable,
     updateStatus
 }
+
 
 /**
  * Create Student 
@@ -26,15 +26,15 @@ module.exports = {
  */
 async function index(req, res) {
     try {
-        let student = await User.find({ "role": 3 }).sort({ first_name: 1 });      
-        let grade = await Grade.find({ "status": 1  }).sort({ 'name': 1 });
+        let student = await User.find({ "role": 3 }).sort({ first_name: 1 });
+        let grade = await Grade.find({ "status": 1 }).sort({ 'name': 1 });
 
-        let totalStudent  = await User.find({ "role": 3 }).sort({ '_id': -1 }).count();
-        let activeStudent  = await User.find({ $and: [ { "role": 3  }, { "status": 1  } ] } ).sort({ '_id': -1 }).count();
-        let deactiveStudent  = await User.find({ $and: [ { "role": 3  }, { "status": 0  } ] } ).sort({ '_id': -1 }).count();
-        const studentObject = {'total':totalStudent,'active':activeStudent,'deactive':deactiveStudent}
+        let totalStudent = await User.find({ "role": 3 }).sort({ '_id': -1 }).count();
+        let activeStudent = await User.find({ $and: [{ "role": 3 }, { "status": 1 }] }).sort({ '_id': -1 }).count();
+        let deactiveStudent = await User.find({ $and: [{ "role": 3 }, { "status": 0 }] }).sort({ '_id': -1 }).count();
+        const studentObject = { 'total': totalStudent, 'active': activeStudent, 'deactive': deactiveStudent }
 
-        return res.render('../views/admin/students/index', { fs:fs,studentObject:studentObject,students:student,grade,grade });
+        return res.render('../views/admin/students/index', { fs: fs, studentObject: studentObject, students: student, grade, grade });
     } catch {
         return res.status(500).json({
             message: 'Internal Server Error'
@@ -42,55 +42,46 @@ async function index(req, res) {
     }
 }
 
-async function datatable(req,res){
+
+/**
+ * dataTable
+ * @param {*} req 
+ * @param {*} res 
+ */
+async function dataTable(req, res) {
     var searchStr = req.body.search.value;
-    const custum_filter = [];
     flag = false;
-    // if(req.body.id !=''){
-    //     flag = true;
-    //     custum_filter.push({'_id':req.body.id});
-    // }
-    // if(req.body.grade !=''){
-    //     custum_filter.push({'grade':req.body.grade});
-    // }
-    // if(req.body.status !=''){
-    //     custum_filter.push({'status':req.body.status});
-    // }
-    // if(flag){
-    //     cusfilter = { $or:custum_filter};
-    // }
-    var obj  = {};
-   // create object for filtering data
-    if(req.body.id){
+    var obj = {};
+
+    if (req.body.id) {
         obj["_id"] = req.body.id;
     }
-    if(req.body.grade){
+    if (req.body.grade) {
         obj["grade_id"] = req.body.grade;
     }
-    if(req.body.status){
+    if (req.body.status) {
         obj["status"] = req.body.status;
     }
+    if (req.body.search.value) {
+        var regex = new RegExp(req.body.search.value, "i")
+        searchStr = { $or: [{ 'first_name': regex }, { 'email': regex }, { 'phone': regex }] };
+    }
+    else {
+        searchStr = {};
+    }
 
-    if(req.body.search.value){
-            var regex = new RegExp(req.body.search.value, "i")
-            searchStr = { $or: [{'first_name': regex },{'email': regex},{'phone':regex}] };
-    }
-    else{
-        searchStr={};
-    }
-    console.log(obj);
-    const filter = ['profile_image','first_name','dial_code','email','status'];
-    const colomn_name = filter[req.body.order[0].column];
-    const order_by =req.body.order[0].dir;
+    const filter = ['profile_image', 'first_name', 'dial_code', 'email', 'status'];
+    const column_name = filter[req.body.order[0].column];
+    const order_by = req.body.order[0].dir;
     var recordsTotal = 0;
-    var recordsFiltered=0;
-    User.count({ "role": 3  }, function(err, c) {
-        recordsTotal=c;
-        User.count({ $and: [ { "role": 3  },obj, searchStr ] }, function(err, c) {
-            recordsFiltered=c;
-            User.find({ $and: [ { "role": 3  },obj,searchStr ] }, '_id profile_image email first_name last_name dial_code phone status',{'skip': Number( req.body.start), 'limit': Number(req.body.length) }, function (err, results) {
+    var recordsFiltered = 0;
+    User.count({ "role": 3 }, function (err, c) {
+        recordsTotal = c;
+        User.count({ $and: [{ "role": 3 }, obj, searchStr] }, function (err, c) {
+            recordsFiltered = c;
+            User.find({ $and: [{ "role": 3 }, obj, searchStr] }, '_id profile_image email first_name last_name dial_code phone status', { 'skip': Number(req.body.start), 'limit': Number(req.body.length) }, function (err, results) {
                 if (err) {
-                    console.log('error while getting results'+err);
+                    console.log('error while getting results' + err);
                     return;
                 }
                 var data = JSON.stringify({
@@ -100,12 +91,12 @@ async function datatable(req,res){
                     "data": results
                 });
                 return res.send(data);
-            }).populate('grade_id').sort({[colomn_name]:order_by});
+            }).populate('grade_id').sort({ [column_name]: order_by });
         });
     });
-    ;
-    // datatable end
 }
+
+
 /**
  * create Student
  * @param {*} req 
@@ -115,14 +106,15 @@ async function datatable(req,res){
 async function create(req, res) {
     try {
         let schools = await School.find({ "status": 1 }).sort({ '_id': -1 });
-        let grade  = await Grade.find( { "status": 1  } );
-        return res.render('../views/admin/students/create', { data: schools,grade:grade });
+        let grade = await Grade.find({ "status": 1 });
+        return res.render('../views/admin/students/create', { data: schools, grade: grade });
     } catch {
         return res.status(500).json({
             message: 'Internal Server Error'
         })
     }
 }
+
 
 /**
  * store Student
@@ -161,12 +153,12 @@ async function edit(req, res) {
     try {
         let StudentId = req.params.id;
         let schools = await School.find({ "status": 1 }).sort({ '_id': -1 });
-        let grade  = await Grade.find( { "status": 1  } );
+        let grade = await Grade.find({ "status": 1 });
         let student = await User.find({ "_id": StudentId });
         let start_date = res.locals.moment(student[0].start_date).format('YYYY-MM-DD');
         let birth_day = res.locals.moment(student[0].birth_day).format('YYYY-MM-DD');
         if (student) {
-            return res.render('../views/admin/students/edit', { data: student[0],grade:grade, school_data: schools, start_date: start_date, birth_day: birth_day, fs: fs });
+            return res.render('../views/admin/students/edit', { data: student[0], grade: grade, school_data: schools, start_date: start_date, birth_day: birth_day, fs: fs });
         }
     } catch {
         return res.status(500).json({
@@ -184,11 +176,10 @@ async function edit(req, res) {
  */
 async function update(req, res) {
     try {
-        console.log(req.body);
         if (req.body.student_id && req.body.student_id != '') {
-            let Student_details = await User.find({"_id": req.body.student_id });
+            let Student_details = await User.find({ "_id": req.body.student_id });
             if (Student_details) {
-              
+
                 studentData = Student_details[0];
                 let profileImage = studentData.profile_image;
                 const filePath = './assets/profileImage/' + profileImage;
@@ -207,7 +198,7 @@ async function update(req, res) {
                     let student = await User.findByIdAndUpdate(req.body.student_id, req.body)
                 } else {
                     delete req.body.profile_image
-                    
+
                     let student = await User.findByIdAndUpdate(req.body.student_id, req.body)
                 }
                 req.flash('success', 'Student is updated successfully!');
@@ -252,6 +243,8 @@ async function destroy(req, res) {
         })
     }
 }
+
+
 /**
  * update status of the Grade.
  * @param {*} req 
@@ -261,8 +254,7 @@ async function destroy(req, res) {
 async function updateStatus(req, res) {
     try {
         if (req.body.uid && req.body.uid != '') {
-           
-            let status = ((req.body.status=='true') ? '1' : '0');
+            let status = ((req.body.status == 'true') ? '1' : '0');
             let tutor = await User.findByIdAndUpdate(req.body.uid, { status: status });
             res.status(200).json({ "success": true, "message": "Student status is updated successfully!" });
         }
